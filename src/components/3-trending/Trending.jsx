b1f452c21911a5064aedb7c6a638e55523d4c60b
amritkuikel <63916236+amritkuikel@users.noreply.github.com>
2023-11-22 22:35:07 +0545

    6

--- src/components/3-trending/Trending.jsx
+++ src/components/3-trending/Trending.jsx
@@ -2,14 +2,13 @@
 import { useState } from "react";
 import Toggle from "./components/Toggle";
 import HorizontalList from "./components/HorizontalList";
-
 export default function Trending() {
   const [movies,setMovies] = useState(true)
   function clickHandler(){
     setMovies(!movies)
   }
   return (
-    <div className=" mt-5 md:mt-10 md:px-20 px-10 flex flex-col items-start gap-5 mx-[5vw] overflow-x-scroll overflow-y-hidden h-[55vh]"  style={{backgroundImage:`url('/assets/images/bg5.jpg')`}} >
+    <div className=" mt-5 md:mt-10 md:px-20 px-10 flex flex-col items-start gap-5 mx-[5vw] overflow-x-scroll overflow-y-hidden h-[55vh]"  style={{backgroundImage:`url('/assets/bg/bg5.jpg')`}} >
 
       <div className="flex justify-center items-center gap-5" >
         <h2 className=" cursor-pointer font-semibold text-2xl">Trending</h2>
