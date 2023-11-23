a43f27c313fbd1982cc4ae4f937420543a3903de
amritkuikel <63916236+amritkuikel@users.noreply.github.com>
2023-11-23 19:33:51 +0545

    9

--- src/components/5-popular/Popular.jsx
+++ src/components/5-popular/Popular.jsx
@@ -2,7 +2,7 @@ import HorizontalList from "./components/HorizontalList";
 
 export default function Popular() {
   return (
-    <div className="mt-5 md:mt-10 md:px-40 px-5 flex flex-col items-start gap-5" style={{backgroundImage:`url('/assets/bg/bg5.jpg')`}}>
+    <div className="mt-5 md:mt-10  px-5 flex flex-col items-start gap-5 mx-[5vw]" style={{backgroundImage:`url('/assets/bg/bg5.jpg')`}}>
 
       <div className="flex justify-center items-center gap-5">
         <h2 className="font-semibold text-2xl">Whats Popular</h2>
