3abcf8d9aba920557f65129bdeda18c5a95437b3
amritkuikel <63916236+amritkuikel@users.noreply.github.com>
2023-11-23 18:03:35 +0545

    8

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,34 +2,34 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className='mt-10 gap-10 py-10 md:py-20 bg-blue-950 flex flex-col md:flex-row text-white justify-center items-center text-center md:text-start md:items-start'>
-        <a href='#'><span className='font-bold text-6xl'>TODFAD</span>TM</a>
+    <div className='mt-10 md:gap-10 gap-4 py-10 md:py-20 bg-blue-950 flex flex-row text-white justify-center items-center text-center md:text-start md:items-start'>
+        <a href='#' className='hidden md:block'><span className='md:font-bold md:text-6xl '>TODFAD</span>TM</a>
         <div className='flex flex-col '>
-            <a href='#' className='font-bold text-xl'>THE BASICS</a>
-            <a href='#'>About TMDB</a>
-            <a href='#'>Contact Us</a>
-            <a href='#'>Support Forums</a>
-            <a href='#'>API</a>
-            <a href='#'>System Status</a>
+            <a href='#' className='md:font-bold md:text-xl font-bold'>BASICS</a>
+            <a href='#' className='hidden md:block'>About TMDB</a>
+            <a href='#' className='hidden md:block'>Contact Us</a>
+            <a href='#' className='hidden md:block'>Support Forums</a>
+            <a href='#' className='hidden md:block'>API</a>
+            <a href='#' className='hidden md:block'>System Status</a>
         </div>
         <div className='flex flex-col'>
-            <a href='#' className='font-bold text-xl'>GET INVOLVED</a>
-            <a href='#'>Contribution Bible</a>
-            <a href='#'>Add New Movie</a>
-            <a href='#'>Add New TV Show</a>
+            <a href='#' className='md:font-bold md:text-xl font-bold'>GET INVOLVED</a>
+            <a href='#' className='hidden md:block'>Contribution Bible</a>
+            <a href='#' className='hidden md:block'>Add New Movie</a>
+            <a href='#' className='hidden md:block'>Add New TV Show</a>
         </div>
         <div className='flex flex-col'>
-            <a href='#' className='font-bold text-xl'>COMMUNITY</a>
-            <a href='#'>Guidelines</a>
-            <a href='#'>Discussions</a>
-            <a href='#'>Leaderboard</a>
+            <a href='#' className='md:font-bold md:text-xl font-bold'>COMMUNITY</a>
+            <a href='#' className='hidden md:block'>Guidelines</a>
+            <a href='#' className='hidden md:block'>Discussions</a>
+            <a href='#' className='hidden md:block'>Leaderboard</a>
         </div>
         <div className='flex flex-col'>
-            <a href='#' className='font-bold text-xl'>LEGAL</a>
-            <a href='#'>Terms of Use</a>
-            <a href='#'>API Terms of Use</a>
-            <a href='#'>Privacy Policy</a>
-            <a href='#'>DMCA Policy</a>
+            <a href='#' className='md:font-bold md:text-xl font-bold'>LEGAL</a>
+            <a href='#' className='hidden md:block'>Terms of Use</a>
+            <a href='#' className='hidden md:block'>API Terms of Use</a>
+            <a href='#' className='hidden md:block'>Privacy Policy</a>
+            <a href='#' className='hidden md:block'>DMCA Policy</a>
         </div>
     </div>
   )
